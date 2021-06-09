59eef360dd52a15424ec2580a3d7e4b7696e07d5
yasserBelatreche <yasserbelatreche000@gmail.com>
2021-06-09 22:10:51 +0100

    add small changes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import moment from "moment";
